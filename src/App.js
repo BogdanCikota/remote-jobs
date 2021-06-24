2859f568f096da233284c9a8eef3de3940be7271
BogdanCikota <bcikota@live.com>
2021-06-24 11:36:55 +0200

    Make pagination within pagination

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import {useEffect, useState} from "react";
 import axios from "axios";
 import Job from "./components/Job";
+import chunk from 'lodash.chunk';
 
 function App() {
-    const [jobs,
-        setJobs] = useState([]);
-    const [isLoading,
-        setIsLoading] = useState(true);
-
+    const [jobs, setJobs] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
     const [fromIndex, setfromIndex] =useState(-1);
     const [toIndex, settoIndex]  = useState(10);
-
     const [pages, setPages] = useState([]);
+    const [chunkedPages, setChunkedPages] = useState([]);
+    const [fromOne, setfromOne] = useState(false);
+    const [fromHundred, setfromHundred] = useState(false);
+    const [fromTwoHundred, setfromTwoHundred] = useState(false);
 
-  
 
     const getJobs = async() => {
         try {
@@ -29,7 +29,7 @@ function App() {
             }
 
             setPages(pagesArr);
-            
+
         } catch (error) {
             console.error(error);
         }
@@ -38,6 +38,10 @@ function App() {
     useEffect(() => {
         getJobs();
     }, []);
+
+    useEffect(() => {
+        setChunkedPages(chunk(pages, 100));
+    }, [pages]);
 
     return (
         <div className="App">
@@ -61,15 +65,48 @@ function App() {
                     }}>prev 10</button>}
 
                     <hr/>
-   
+
+                    <div>
+                        <h2>pages</h2>
+                        <button onClick={()=>{
+                            setfromOne(prev=>!prev);
+                            setfromHundred(false);
+                            setfromTwoHundred(false)
+                        }}>1...100,</button>
+                        <button onClick={()=>{
+                            setfromHundred(prev=>!prev);
+                            setfromOne(false);
+                        }}>100...200,</button>
+                        <button onClick={()=>{
+                            setfromTwoHundred(prev=>!prev)
+                            setfromOne(false);
+                            setfromHundred(false)
+                        }}>200...</button>
+                    </div>
+
+                    <hr />
 
                     {
-                        pages.map((page, index) => <button className='inline-block' key={index} onClick={()=>{
-                            setfromIndex(page*10-1);
-                            settoIndex(page*10+10);
-                        }}>{page+1},</button> )
+                       fromOne ?  chunkedPages.map((chunk, index)=> index === 0 && <div className='inline-block' key={index}>{chunk.map((page, index) => {
+                        return <button className='inline-block' key={index} onClick={()=>{
+                               setfromIndex(page*10-1);
+                               settoIndex(page*10+10);
+                           }}>{page+1},</button>
+                       } )},</div> ) :
+                        fromHundred ? chunkedPages.map((chunk, index)=> index === 1 && <div className='inline-block' key={index}>{chunk.map((page, index) => {
+                            return <button className='inline-block' key={index} onClick={()=>{
+                                   setfromIndex(page*10-1);
+                                   settoIndex(page*10+10);
+                               }}>{page+1},</button>
+                           } )},</div> ) :
+                        fromTwoHundred && chunkedPages.map((chunk, index)=> index === 2 && <div className='inline-block' key={index}>{chunk.map((page, index) => {
+                            return <button className='inline-block' key={index} onClick={()=>{
+                                   setfromIndex(page*10-1);
+                                   settoIndex(page*10+10);
+                               }}>{page+1},</button>
+                           } )},</div> )
                     }
-                    
+
                     <hr/>
                     {toIndex > jobs.length-1 ? <button disabled>next 10</button> :
                      <button onClick={()=>{
@@ -78,6 +115,7 @@ function App() {
                     }}>next 10</button>}
                     
                 </div>}
+
         </div>
     );
 }

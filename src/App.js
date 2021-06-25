eba7f422bd8406da0f04d053b9bf64dbbf9907dc
BogdanCikota <bcikota@live.com>
2021-06-25 11:54:16 +0200

    Choose a job category

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ function App() {
     const [pagesCounter, setpagesCounter] = useState(0);
     const [pages, setPages] = useState([]);
     const [limit, setLimit] = useState('all');
+    const [categories, setCategories] = useState([]);
+    const [category, setCategory] = useState('');
 
     const getJobs = async() => {
         try {
-            const response = await axios.get(`https://remotive.io/api/remote-jobs?limit=${limit}`);
+            const response = await axios.get(`https://remotive.io/api/remote-jobs?limit=${limit}&category=${category}`);
             console.log(response.data.jobs);
             setJobs(response.data.jobs);
             setIsLoading(false);
@@ -31,9 +33,23 @@ function App() {
         }
     };
 
+
+    const getCategories = async() => {
+        try {
+            const response = await axios.get('https://remotive.io/api/remote-jobs/categories');
+            console.log(response.data.jobs);
+            setCategories(response.data.jobs);
+
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
     useEffect(() => {
         getJobs();
-    }, [limit]);
+        getCategories();
+    }, [limit, category]);
 
     
 
@@ -49,6 +65,16 @@ function App() {
                 setLimit(e.target.value);
                 setIsLoading(true);
             }} />
+
+            <br />
+            
+            <select onChange={(e)=>{
+                setCategory(e.target.value);
+                setIsLoading(true);
+            }}>
+                <option>Choose a job category</option>
+                {categories.map( (category, index) => <option key={index} value={category.slug} >{category.name}</option> )}
+            </select>
           
             {isLoading
                 ? <h2>Loading...</h2>

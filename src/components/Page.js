57cb2f07c3e693cba6e91aee3a31f6be98dc906b
BogdanCikota <bcikota@live.com>
2021-06-29 14:56:48 +0200

    Style Pagination

--- src/components/Page.js
+++ src/components/Page.js
@@ -2,13 +2,13 @@ import React from 'react'
 
 function Page({chunk, setPageNum, setfromIndex, settoIndex}) {
     return (
-        <div className='inline-block' >{chunk.map((page, index) => {
-            return <button className='inline-block' key={index} onClick={()=>{
+        <div className='p-2 grid grid-cols-8 gap-2 col-span-full' >{chunk.map((page, index) => {
+            return <button className='rounded-lg px-1  bg-blue-400 text-white' key={index} onClick={()=>{
                     setPageNum(page+1);
                     setfromIndex(page*10-1);
                     settoIndex(page*10+10);
-                }}>{page+1},</button>
-            } )},</div>
+                }}>{page+1}</button>
+            } )}</div>
     )
 }
 
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -7,9 +7,10 @@ function Pagination({jobs, fromIndex, setPageNum, setfromIndex, settoIndex, toIn
     const [fromTwoHundred, setfromTwoHundred] = useState(false);
 
     return (
-        <div>
-            {fromIndex === -1 ? <button disabled>prev 10</button> : 
-            <button onClick={()=>{
+        <div className='grid gap-2'>
+            <div className='grid grid-cols-2'>
+            {fromIndex === -1 ? <button className='ml-2 rounded-lg px-1  bg-blue-500 text-white' disabled>prev 10</button> : 
+            <button className='ml-2 rounded-lg px-1  bg-blue-500 text-white' onClick={()=>{
                 setPageNum(()=>{
                     return document.querySelector('.pageNum').innerHTML-1
                 });
@@ -17,20 +18,32 @@ function Pagination({jobs, fromIndex, setPageNum, setfromIndex, settoIndex, toIn
                 settoIndex(toIndex-10);
             }}>prev 10</button>}
 
-            <hr/>
+            {toIndex > jobs.length-1 ? <button className='ml-2 rounded-lg px-1  bg-blue-500 text-white' disabled>next 10</button> :
+                <button className='ml-2 rounded-lg px-1  bg-blue-500 text-white' onClick={()=>{
+                setPageNum(()=>{
+                    return +document.querySelector('.pageNum').innerHTML + +1
+                    });
+                setfromIndex(fromIndex+10);
+                settoIndex(toIndex+10);
+            }}>next 10</button>}
 
-            {pagesCounter > 100 && <div>
-                <h2>pages</h2>
-                <button onClick={()=>{
+            {
+                pagesCounter < 100 && chunkedPages.map((chunk, index)=> index === 0 && <Page  key={index} chunk={chunk} setPageNum={setPageNum} setfromIndex={setfromIndex} settoIndex={settoIndex} /> )
+            }
+
+            </div>
+            
+            {pagesCounter > 100 && <div className='grid grid-cols-3'>
+                <button className='ml-2 rounded-lg px-1  bg-blue-500 text-white' onClick={()=>{
                     setfromOne(prev=>!prev);
                     setfromHundred(false);
                     setfromTwoHundred(false)
-                }}>1...100,</button>
-                {pagesCounter > 100 && <button onClick={()=>{
+                }}>1...100</button>
+                {pagesCounter > 100 && <button className='ml-2 rounded-lg px-1  bg-blue-500 text-white' onClick={()=>{
                     setfromHundred(prev=>!prev);
                     setfromOne(false);
-                }}>100...200,</button>}
-                {pagesCounter > 200 &&<button onClick={()=>{
+                }}>100...200</button>}
+                {pagesCounter > 200 &&<button className='ml-2 rounded-lg px-1  bg-blue-500 text-white' onClick={()=>{
                     setfromTwoHundred(prev=>!prev)
                     setfromOne(false);
                     setfromHundred(false)
@@ -45,15 +58,8 @@ function Pagination({jobs, fromIndex, setPageNum, setfromIndex, settoIndex, toIn
                 fromTwoHundred && chunkedPages.map((chunk, index)=> index === 2 && <Page key={index} chunk={chunk} setPageNum={setPageNum} setfromIndex={setfromIndex} settoIndex={settoIndex} />)
             }
 
-            <hr/>
-            {toIndex > jobs.length-1 ? <button disabled>next 10</button> :
-                <button onClick={()=>{
-                setPageNum(()=>{
-                    return +document.querySelector('.pageNum').innerHTML + +1
-                    });
-                setfromIndex(fromIndex+10);
-                settoIndex(toIndex+10);
-            }}>next 10</button>}
+            
+           
         </div>
     )
 }

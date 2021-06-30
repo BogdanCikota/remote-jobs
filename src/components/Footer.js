dd64e5c59a0ac526814195899202c66252f88be3
BogdanCikota <bcikota@live.com>
2021-06-30 10:09:44 +0200

    Improve Footer design

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 function Footer() {
     return (
-        <footer className='bg-blue-500 p-3 pt-5 text-white mt-2'>
-            <p>API Source: <a href="https://remotive.io/" rel="noreferrer" target="_blank">https://remotive.io/api-documentation</a></p>
-            <p className='text-center'>Made by <a className='underline' href="https://github.com/BogdanCikota" rel="noreferrer" target="_blank">Bogdan</a> </p>
+        <footer className='bg-blue-400 grid gap-2 text-white mt-2 text-center'>
+            <p className='py-2'>API Source: <a href="https://remotive.io/" rel="noreferrer" target="_blank">https://remotive.io/api-documentation</a></p>
+            <p className='bg-blue-500 py-2'>Made by <a className='underline' href="https://github.com/BogdanCikota" rel="noreferrer" target="_blank">Bogdan</a> </p>
         </footer>
     )
 }

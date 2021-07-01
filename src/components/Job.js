e7ade75020e4d37d43fe13e566dc049535de3010
BogdanCikota <bcikota@live.com>
2021-07-01 11:40:37 +0200

    Add goToTop function when clicked Read more...

--- src/components/Job.js
+++ src/components/Job.js
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom';
 
 function Job({job, index}) {
 
+    const goToTop = () => {
+        window.scroll(0,0);
+    }
+
     return (
         <div className={index%2 !== 0 ? 'bg-gray-100 p-3': 'bg-gray-50 p-3'}>
             <h3 className='text-blue-900 font-semibold'>Company: <span className='font-bold' >{job.company_name}</span> </h3>
@@ -10,7 +14,7 @@ function Job({job, index}) {
             <div className='text-blue-900 font-semibold'>Required location: {job.candidate_required_location === '' ? <span>/</span> : <span className='font-normal'>{job.candidate_required_location}</span> } </div>
             <div className='text-blue-900 font-semibold'>Job Type: {job.job_type ? <span className='font-normal'>{job.job_type}</span> : <span>/</span> } </div>
             <div className='text-blue-900 font-semibold'>Published: <span className='font-normal'>{job.publication_date}</span> </div>
-            <Link className='text-blue-900 font-semibold' to={{pathname:"/JobDescription", state: {description: job.description}}}>Read more...</Link>
+            <Link onClick={goToTop} className='text-blue-900 font-semibold' to={{pathname:"/JobDescription", state: {description: job.description}}}>Read more...</Link>
         </div>
     )
 }

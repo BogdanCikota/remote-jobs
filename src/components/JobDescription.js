65d2270cc3645378dadaa625aac5e55baff2a239
BogdanCikota <bcikota@live.com>
2021-07-02 12:50:07 +0200

    Improve JobDescription design

--- src/components/JobDescription.js
+++ src/components/JobDescription.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom';
 function JobDescription() {
     const location = useLocation();
     const {description} = location.state;
+    
     return (
-       <div>
+       <div className='bg-gray-50'>
         <Link to='/' className="underline text-blue-900 font-semibold"> &#8592; back</Link>
-        <div className='job-description' dangerouslySetInnerHTML={{ __html: description }}></div>
+        <div className='job-description p-2 text-blue-900 grid gap-2' dangerouslySetInnerHTML={{ __html: description }}></div>
+        <Link to='/' className="underline text-blue-900 font-semibold"> &#8592; back</Link>
        </div>
     )
 }

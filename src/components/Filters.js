67d9d00ea41a1f6caa3025ad12c0326a7cc6629b
BogdanCikota <bcikota@live.com>
2021-07-07 13:47:37 +0200

    Put Limit input in form and add go! button

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Filters({goToPage, setgoToPage, numOfResults, setfromIndex, settoIndex, setPageNum, categories, setLimit, setCategory, setSearch, inputSearch, setInputSearch, setIsLoading}) {
+    const [inputLimit, setInputLimit] = useState(0);
     const resetPage = () => {
-        setfromIndex(0); settoIndex(numOfResults); setPageNum(1); setgoToPage(!goToPage);
+        setfromIndex(0); settoIndex(numOfResults); setPageNum(1); setgoToPage(false);
     };
     return (
         <div className='filters grid gap-3'>
@@ -30,14 +31,20 @@ function Filters({goToPage, setgoToPage, numOfResults, setfromIndex, settoIndex,
                 <button className='ml-2 rounded-lg px-1  bg-blue-500 text-white' type="submit">Go!</button>
             </form>
 
-           <div className='border p-2 grid gap-1'>
-            <label  htmlFor="limit">Limit the number of job listing result</label>
-                <input className='border w-20' type="number" id="limit" min="1" onChange={ e => {
-                    setLimit(e.target.value);
-                    setIsLoading(true);
-                    resetPage();
-                }} /> <span>...will not work if all tree filters are selected, backend problem :(</span>
-           </div>
+           <form onSubmit={e => {
+                e.preventDefault();
+                setLimit(inputLimit);
+                setIsLoading(true);
+                setInputLimit(0);
+                resetPage();
+                document.querySelector("#limit").value = '';
+           }} className='border p-2 '>
+            <label className='block'  htmlFor="limit">Limit the number of job listing result <span className='text-sm italic'>(will not work with search input, API problem)</span></label>
+                <input className='border w-20 my-1.5' type="number" id="limit" min="1" onChange={ e => {
+                    setInputLimit(e.target.value);
+                }} /><button className='ml-2 rounded-lg px-1  bg-blue-500 text-white' type="submit">Go!</button> 
+                
+           </form>
         </div>
     )
 }

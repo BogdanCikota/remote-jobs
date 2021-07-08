95fa46cd250f9c6a07746e94e267dda4d83eb533
BogdanCikota <bcikota@live.com>
2021-07-08 15:25:17 +0200

    Refresh page when h1 is clicked

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ function Header({ openFilters, setOpenFilters }) {
     const filters = document.querySelector('.filters');
     return (
         <header className="flex bg-blue-400 p-3 pb-5">
-            <h1 className=' text-white text-2xl text-center'>Remote Jobs</h1>
+            <h1 className=' text-white text-2xl text-center cursor-pointer' onClick={()=> window.location.reload()}>Remote Jobs</h1>
             <button className='block  mt-2 bg-blue-500 text-white rounded-full  px-3 ml-auto' onClick={()=> {
                 setOpenFilters(openFilters => !openFilters);
                 !openFilters? filters.style = `display: grid`:filters.style = `display: none`

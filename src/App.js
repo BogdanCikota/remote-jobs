e50cd5e7a84e434094f6a0132d1a4a231ad89a36
BogdanCikota <bcikota@live.com>
2021-07-09 17:33:04 +0200

    Revert "Move found results to Header"
    This reverts commit 33944efa845ce2c4593c3a53d8731b09fa7bee2c.

--- src/App.js
+++ src/App.js
@@ -74,7 +74,6 @@ function App() {
         <HashRouter basemname='/'>
             <div className="max-w-sm m-auto">
                 <Header 
-                jobs={jobs}
                 openFilters={openFilters} 
                 setOpenFilters={setOpenFilters}
                 setLimit={setLimit} 

669e75a7ccc65277655c0c33a7209f8a65b0312d
BogdanCikota <bcikota@live.com>
2021-07-12 13:03:11 +0200

    Improve JobList ux/ui

--- src/components/JobList.js
+++ src/components/JobList.js
@@ -13,16 +13,16 @@ function JobList({goToPage, setgoToPage, pages, jobs, pagesCounter, fromIndex, s
     }, [pages, setChunkedPages]);
 
     return (
-        <div className='grid gap-2 mb-2 xl:mt-16 xl:m-auto xl:max-w-5xl'>
-            <div className='text-center bg-blue-500 p-1 text-white xl:bg-blue-300 xl:rounded-b-full xl:p-1.5'>Found <span>{jobs.length} results</span></div>
-            <h2 className='font-bold text-center p-1 pl-3 rounded-full  px-3 bg-gray-100 inline-block text-blue-900' >Page <span className='pageNum'>{pageNum}</span> </h2>                    
-            <div>
+        <div className='grid gap-0.5 mb-2 xl:mt-16 xl:m-auto xl:max-w-5xl'>
+            <div className='text-center bg-blue-500 p-1 text-white xl:bg-blue-300 xl:rounded-b-full xl:p-1.5 xl:mb-3'>Found <span>{jobs.length} results</span></div>
+            <h2 className='font-bold text-center p-1 pl-3 xl:rounded-t-3xl px-3 bg-gray-100 inline-block text-blue-900' >Page <span className='pageNum'>{pageNum}</span> </h2>                    
+            <div className='grid gap-0.5'>
                 {jobs.map((job, index) => {
                     return index >= fromIndex && index < toIndex && <Job key={index} index={index} job={job}/>
                 })}
             </div>
 
-            <h2 className='font-bold text-center p-1 pl-3 rounded-full  px-3 bg-gray-100 inline-block text-blue-900' >Page <span className='pageNum'>{pageNum}</span> </h2>
+            <h2 className='font-bold text-center p-1 pl-3 xl:rounded-b-3xl  px-3 bg-gray-100 inline-block text-blue-900' >Page <span className='pageNum'>{pageNum}</span> </h2>
             
             {
                 jobs.length > numOfResults  && 
--- src/components/jobList_components/Job.js
+++ src/components/jobList_components/Job.js
@@ -5,18 +5,23 @@ function Job({job, index}) {
 
     const goToTop = () => {
         window.scroll(0,0);
-    }
+    } 
 
     return (
-        <div className={`${index%2 !== 0 ? `bg-gray-100`: `bg-gray-50`} p-3 lg:text-lg lg:p-4 lg:pl-6`}>
-            <img src={`https://remotive.io/job/${job.id}/logo`} alt={`logo ${job.company_name}`} className='rounded-full w-16 float-left mr-4  lg:w-auto' />
-            <h3 className='text-blue-900 font-semibold'>Company: <span className='font-bold' >{job.company_name}</span> </h3>
-            <p className='text-blue-900 font-semibold'>Category: <span className='font-normal'>{job.category}</span> </p>
-            <div className='text-blue-900 font-semibold'>Required location: {job.candidate_required_location === '' ? <span>/</span> : <span className='font-normal'>{job.candidate_required_location}</span> } </div>
-            <div className='text-blue-900 font-semibold'>Job Type: {job.job_type ? <span className='font-normal'>{job.job_type}</span> : <span>/</span> } </div>
-            <div className='text-blue-900 font-semibold'>Published: <span className='font-normal'>{job.publication_date}</span> </div>
-            <Link onClick={goToTop} className='text-blue-900 font-semibold' to={{pathname:"/JobDescription", state: {description: job.description}}}>Read more...</Link>
-        </div>
+        <Link onClick={goToTop} className={`bg-gray-50 hover:bg-gray-100 p-4 block xl:text-lg xl:p-4 xl:pl-6 lg:flex justify-between`} to={{pathname:"/JobDescription", state: {description: job.description}}}>
+            <div className='flex gap-4 xl:gap-5'>
+                <img src={`https://remotive.io/job/${job.id}/logo`} alt={`logo ${job.company_name}`} className='rounded-full w-16 h-16' />
+                <div>
+                    <h3 className='text-blue-400'>{job.company_name}</h3>
+                    <h2 className='text-blue-900 font-semibold'>{job.title}</h2>
+                </div>
+            </div>
+
+            <div className='hidden lg:block text-blue-400 text-right'>
+                <p>{job.category}</p>
+                <div>{job.candidate_required_location}</div>
+            </div>
+        </Link>
     )
 }
 

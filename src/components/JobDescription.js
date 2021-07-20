78508dc42600b6962b984f7f94607b51bd0d7a87
BogdanCikota <bcikota@live.com>
2021-07-20 23:10:46 +0200

    Improve JobDescription ui

--- src/components/JobDescription.js
+++ src/components/JobDescription.js
@@ -23,9 +23,9 @@ function JobDescription() {
     
     return (
        <div className='xl:mt-16 xl:m-auto xl:max-w-5xl xl:p-4 text-gray-800'>
-        <Link to='/' className="underline font-semibold lg:text-lg"> &#8592; back</Link>
+        <Link to='/' className="ml-2 underline font-semibold lg:text-lg"> &#8592; back</Link>
         
-        <div className='xl:flex gap-2'>
+        <div className='xl:flex gap-2 px-4 xl:px-0'>
             <div className='w-20 h-20 m-auto xl:border xl:p-3 xl:m-0 xl:w-auto xl:h-auto self-center'>
                 <img className='rounded-full xl:rounded-none' src={`https://remotive.io/job/${job.id}/logo`} alt={`logo ${job.company_name}`} />
             </div>
@@ -42,8 +42,8 @@ function JobDescription() {
             </div>
         </div>
 
-        <div className='border job-description py-3 px-6 w-screen overflow-scroll sm:w-full sm:overflow-visible grid gap-4' dangerouslySetInnerHTML={{ __html: job.description }}></div>
-        <Link to='/' className="underline font-semibold lg:text-lg"> &#8592; back</Link>
+        <div className='border job-description py-3 px-5 w-screen overflow-scroll sm:w-full sm:overflow-visible grid gap-4 leading-relaxed' dangerouslySetInnerHTML={{ __html: job.description }}></div>
+        <Link to='/' className="ml-2 underline font-semibold lg:text-lg"> &#8592; back</Link>
        </div>
     )
 }

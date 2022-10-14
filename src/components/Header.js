7887095412c076361dff63a135e5113cf8ea180c
Bogdan Cikota <bcikota@live.com>
2022-10-14 17:28:03 +0200

    Migrate to Redux

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,51 @@
+import { useDispatch, useSelector } from "react-redux";
 import Filters from "./header_components/Filters";
+import { setOpenFilters, setJobPositionTop } from "../redux/features/globalSlice";
 
-function Header({setJobPositionTop, openFilters, setOpenFilters, setgoToPage, numOfResults, setNumOfResults, setfromIndex, settoIndex, setPageNum, categories, setLimit, setCategory, setSearch, inputSearch, setInputSearch, setIsLoading }) {
+function Header() {
+  const dispatch = useDispatch();
+  const globalState = useSelector((store) => store["global"]);
+  const { openFilters } = globalState;
+  return (
+    <header
+      className={`${
+        !openFilters ? `pb-3` : `md:pb-4 md:bg-opacity-60`
+      } bg-blue-400 grid grid-cols-3 md:grid-cols-7 md:fixed w-full left-0 top-0 md:px-7  md:gap-3`}
+    >
+      <h1
+        className="col-span-2 m-3 mb-2  text-white text-2xl cursor-pointer"
+        onClick={() => window.location.reload()}
+      >
+        Remote Jobs
+      </h1>
+      <button
+        className={`${
+          openFilters ? `bg-opacity-0 mr-6 md:self-start md:mt-2` : `mt-3`
+        } mr-3  rounded-2xl px-2 py-1 col-start-3 justify-self-end self-center bg-blue-500 text-white  md:col-start-7 md:px-3`}
+        onClick={() => {
+          dispatch(setJobPositionTop(0));
+          dispatch(setOpenFilters(openFilters));
+        }}
+      >
+        {" "}
+        {openFilters ? (
+          <span className="text-xl md:text-2xl">x</span>
+        ) : (
+          <span>Filters</span>
+        )}
+      </button>
 
-    return (
-        <header className={`${!openFilters ? `pb-3` : `md:pb-4 md:bg-opacity-60`} bg-blue-400 grid grid-cols-3 md:grid-cols-7 md:fixed w-full left-0 top-0 md:px-7  md:gap-3`}>
-            <h1 className='col-span-2 m-3 mb-2  text-white text-2xl cursor-pointer' onClick={()=> window.location.reload()}>Remote Jobs</h1>
-            <button className={`${openFilters ? `bg-opacity-0 mr-6 md:self-start md:mt-2` : `mt-3` } mr-3  rounded-2xl px-2 py-1 col-start-3 justify-self-end self-center bg-blue-500 text-white  md:col-start-7 md:px-3`} 
-            onClick={()=> {
-                setJobPositionTop(0);
-                setOpenFilters(openFilters => !openFilters);
-                }}> {openFilters ? <span className='text-xl md:text-2xl'>x</span> : <span >Filters</span> }  
-            </button>
-            
-            <div className={`${openFilters ? `col-span-full md:col-start-2 md:col-end-7  xl:col-start-3 xl:col-end-6 xl:row-span-full` : `hidden`} md:mt-5 md:mb-1 `}>
-                <Filters 
-                setLimit={setLimit} 
-                setCategory={setCategory} 
-                setSearch={setSearch}
-                inputSearch={inputSearch}
-                setIsLoading={setIsLoading}
-                setInputSearch={setInputSearch}
-                categories={categories}
-                setfromIndex={setfromIndex}
-                settoIndex={settoIndex}
-                setPageNum={setPageNum}
-                numOfResults={numOfResults}
-                setNumOfResults={setNumOfResults}
-                setgoToPage={setgoToPage}
-                setOpenFilters={setOpenFilters}
-                />
-            </div>
-            
-        </header>
-    )
+      <div
+        className={`${
+          openFilters
+            ? `col-span-full md:col-start-2 md:col-end-7  xl:col-start-3 xl:col-end-6 xl:row-span-full`
+            : `hidden`
+        } md:mt-5 md:mb-1 `}
+      >
+        <Filters />
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
--- src/components/jobList_components/Pagination.js
+++ src/components/jobList_components/Pagination.js
@@ -2,105 +2,93 @@ import Pages from "./pagination_components/Pages";
 import PrevButton from "./pagination_components/PrevButton";
 import NextButton from "./pagination_components/NextButton";
 import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setfromIndex, setgoToPage, setJobPositionTop, settoIndex, setPageNum } from "../../redux/features/globalSlice";
 
-function Pagination({setJobPositionTop, goToPage, setgoToPage, numOfResults, jobs, fromIndex, setPageNum, setfromIndex, settoIndex, toIndex, pages, chunkedPages }) {
-    const [paginationNums, setPaginationNums] = useState(0);
-    const pageNumElement = document.querySelector('.pageNum');
+function Pagination() {
+  const [paginationNums, setPaginationNums] = useState(0);
+  const pageNumElement = document.querySelector(".pageNum");
+  const globalState = useSelector((store) => store["global"]);
+  const { pages, chunkedPages, goToPage, numOfResults } = globalState;
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        if(window.innerWidth > 425) {
-            setPaginationNums(11);
-        } else {
-            setPaginationNums(5);
-        }
-    }, [])
-    
+  useEffect(() => {
+    if (window.innerWidth > 425) {
+      setPaginationNums(11);
+    } else {
+      setPaginationNums(5);
+    }
+  }, []);
 
-    return (
-            <div className='m-auto my-4 flex gap-6 justify-center mb-1.5 items-center px-4'>
-                <PrevButton 
-                setPageNum={setPageNum}
-                fromIndex={fromIndex} 
-                setfromIndex={setfromIndex}
-                toIndex={toIndex}
-                settoIndex={settoIndex}
-                pageNumElement={pageNumElement}
-                numOfResults={numOfResults}
-                setJobPositionTop={setJobPositionTop}
-                />
-                {pages && pages.length > 100 && !goToPage && <button onClick={(e)=> {
-                     setgoToPage(!goToPage);
-                     setJobPositionTop(e.target.offsetTop);
-                    }} className='rounded-lg px-1 bg-blue-400 hover:bg-blue-500 text-white'>Go to page...</button>}
+  return (
+    <div className="m-auto my-4 flex gap-6 justify-center mb-1.5 items-center px-4">
+      <PrevButton
+        pageNumElement={pageNumElement}
+      />
+      {pages && pages.length > 100 && !goToPage && (
+        <button
+          onClick={(e) => {
+            dispatch(setgoToPage(!goToPage));
+            dispatch(setJobPositionTop(e.target.offsetTop));
+          }}
+          className="rounded-lg px-1 bg-blue-400 hover:bg-blue-500 text-white"
+        >
+          Go to page...
+        </button>
+      )}
 
-            { 
-                // generate from/to select
-                goToPage && pages.length > 100 && 
-                <div className='flex gap-3 flex-wrap justify-center'>
-                    {
-                        chunkedPages.map((chunk, index) => {
-                            return <Pages
-                                    key={index}
-                                    pages={pages}
-                                    lastChunk={chunkedPages.length-1}
-                                    chunkIndex={index}
-                                    chunk={chunk}
-                                    setPageNum={setPageNum}
-                                    setfromIndex={setfromIndex}
-                                    settoIndex={settoIndex}
-                                    numOfResults={numOfResults}
-                                    setJobPositionTop={setJobPositionTop}
-                                    />
-                        })
-                    }
-                </div>
-            }
-
-            {
-               chunkedPages.length > 0 &&  pages.length < 100 && pages.length >= paginationNums &&
+      {
+        // generate from/to select
+        goToPage && pages.length > 100 && (
+          <div className="flex gap-3 flex-wrap justify-center">
+            {chunkedPages.map((chunk, index) => {
+              return (
                 <Pages
-                chunk={chunkedPages[0]}
-                setPageNum={setPageNum} 
-                setfromIndex={setfromIndex} 
-                settoIndex={settoIndex}
-                numOfResults={numOfResults}
-                setJobPositionTop={setJobPositionTop}
-                /> 
-            }
+                  key={index}
+                  lastChunk={chunkedPages.length - 1}
+                  chunkIndex={index}
+                  chunk={chunk}
+                />
+              );
+            })}
+          </div>
+        )
+      }
 
-            {
-                chunkedPages.length > 0 &&  pages.length < paginationNums && 
-                <div className='flex flex-wrap gap-2 justify-center'>
-                    {chunkedPages[0].map((num, index) => 
-                    <button 
-                    key={index} 
-                    className={`${num+1 === 10 && `pr-0.5`} rounded-full w-8 py-1  bg-blue-400 hover:bg-blue-500 text-white` }
-                    onClick={()=>{
-                        setPageNum(num+1);
-                        setfromIndex(num * numOfResults);
-                        settoIndex(+num * numOfResults + +numOfResults);
-                        setJobPositionTop(0);
-                    }}
-                    >
-                        {num+1}
-                    </button>
-                    )}
-                </div>
-            }
+      {chunkedPages.length > 0 &&
+        pages.length < 100 &&
+        pages.length >= paginationNums && (
+          <Pages
+            chunk={chunkedPages[0]}
+          />
+        )}
 
-            <NextButton
-            setPageNum={setPageNum}
-            fromIndex={fromIndex} 
-            setfromIndex={setfromIndex}
-            toIndex={toIndex}
-            settoIndex={settoIndex}
-            pageNumElement={pageNumElement}
-            numOfResults={numOfResults}
-            jobs={jobs}
-            setJobPositionTop={setJobPositionTop}
-            />
+      {chunkedPages.length > 0 && pages.length < paginationNums && (
+        <div className="flex flex-wrap gap-2 justify-center">
+          {chunkedPages[0].map((num, index) => (
+            <button
+              key={index}
+              className={`${
+                num + 1 === 10 && `pr-0.5`
+              } rounded-full w-8 py-1  bg-blue-400 hover:bg-blue-500 text-white`}
+              onClick={() => {
+                dispatch(setPageNum(num + 1));
+                dispatch(setfromIndex(num * numOfResults));
+                dispatch(settoIndex(+num * numOfResults + +numOfResults));
+                dispatch(setJobPositionTop(0));
+              }}
+            >
+              {num + 1}
+            </button>
+          ))}
         </div>
-    )
+      )}
+
+      <NextButton
+        pageNumElement={pageNumElement}
+      />
+    </div>
+  );
 }
 
-export default Pagination
+export default Pagination;
